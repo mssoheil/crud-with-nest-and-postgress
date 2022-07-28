b94dd76f048c03500a66d8bfe919eb7223db7dbd
mssoheil <mssoheil88@yahoo.com>
2022-07-28 18:59:11 +0430

    refactor: refactor the auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,10 +16,65 @@ dotenv.config();
 @Injectable()
 export class AuthService {
   users: User[] = [];
-  async createUser(userName: string, password: string) {
+
+  private handleBadRequest(userName: string, password: string) {
     if (!userName || !password) {
-      throw new BadRequestException('Provide the userName and password');
+      throw new BadRequestException('Provide userName and password');
     }
+  }
+
+  private handleUserHasConflict(userName: string) {
+    if (this.isUserExist(userName)) {
+      throw new ConflictException('The user exists');
+    }
+  }
+
+  private handleUserNotFound(userName: string) {
+    const userIsExist = this.isUserExist(userName);
+
+    if (!userIsExist) {
+      throw new UnauthorizedException('The userName or password is wrong');
+    }
+  }
+
+  private isUserExist(userName: string): boolean {
+    return !!this.findUserByUserName(userName);
+  }
+
+  private findUserByUserName(userName: string): User | null {
+    const user = this.users.find((item: User) => item.userName === userName);
+    if (!user) {
+      return null;
+    }
+    return user;
+  }
+
+  private createAccessToken(user: User) {
+    const userWithoutPassword = {
+      userName: user.userName,
+      id: user.id,
+    };
+
+    const accessToken = jwt.sign(
+      userWithoutPassword,
+      process.env.ACCESS_TOKEN_SECRET_KEY,
+    );
+
+    return accessToken;
+  }
+
+  private async handlePasswordNotMatched(user: User, password: string) {
+    const passwordDoesMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordDoesMatch) {
+      throw new UnauthorizedException('The userName or password is wrong');
+    }
+  }
+
+  async createUser(userName: string, password: string) {
+    this.handleBadRequest(userName, password);
+
+    this.handleUserHasConflict(userName);
 
     if (this.isUserExist(userName)) {
       throw new ConflictException('The user exists');
@@ -38,50 +93,20 @@ export class AuthService {
     return 'User created';
   }
 
-  isUserExist(userName: string): boolean {
-    return !!this.findUserByUserName(userName);
-  }
-
-  findUserByUserName(userName: string): User | null {
-    const user = this.users.find((item: User) => item.userName === userName);
-    if (!user) {
-      return null;
-    }
-    return user;
-  }
-
   getAllUsers(): User[] {
     return [...this.users.map((item) => item)];
   }
 
   async loginUser(userName: string, password: string) {
-    if (!userName || !password) {
-      throw new BadRequestException('Provide userName and password');
-    }
+    this.handleBadRequest(userName, password);
 
-    const userIsExist = this.isUserExist(userName);
-
-    if (!userIsExist) {
-      throw new UnauthorizedException('The userName or password is wrong');
-    }
+    this.handleUserNotFound(userName);
 
     const user = this.findUserByUserName(userName);
 
-    const passwordDoesMatch = await bcrypt.compare(password, user.password);
+    this.handlePasswordNotMatched(user, password);
 
-    if (!passwordDoesMatch) {
-      throw new UnauthorizedException('The userName or password is wrong');
-    }
-
-    const userWithoutPassword = {
-      userName: user.userName,
-      id: user.id,
-    };
-
-    const accessToken = jwt.sign(
-      userWithoutPassword,
-      process.env.ACCESS_TOKEN_SECRET_KEY,
-    );
+    const accessToken = this.createAccessToken(user);
 
     return { accessToken };
   }
